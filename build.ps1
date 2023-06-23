376027efe70567c575c84650b072c6559d667aa1
jimgodden <jgodden90@outlook.com>
2023-06-23 08:50:41 -0400

    adding deploy to azure button in readme

--- build.ps1
+++ build.ps1
@@ -1,1 +1,9 @@
 bicep build .\main.bicep
+
+$originalURL = "https://github.com/jimgodden/AppGW_to_ASE/blob/main/main.json"
+$shortURL = $originalURL.Substring(14)
+$rawURL = "https://raw.githubusercontent${shortURL}"
+$encodedURL = [uri]::EscapeDataString($rawURL)
+
+Write-Host "Below is the string needed for the Deploy to Azure button in a readme.md file"
+Write-host "[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/${encodedURL})"
